004ea1f62a3673713b26df2f27edbff454da2d72
marcokebonte <dkebonte@gmail.com>
2023-06-03 00:16:38 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/oh_sheet/CreateTimesheetActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.oh_sheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateTimesheetActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_timesheet2)
+    }
+}
---
+++ app/src/main/java/com/example/oh_sheet/ManageTimesheetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oh_sheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageTimesheetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_timesheet)
+    }
+}
